097fdca2083e4968dc8216f7985c5ff9ff527a76
Rodrigo Bossini <professorbossini@gmail.com>
2025-03-08 12:55:35 -0300

    learn(04-react): encerra o codigo 2.8.1
    - fizemos  a definicao do componente cartao
    - o pedido ainda nao teve a definicao de cartao removida. comecar por ai

---
+++ componentes-props/src/Cartao.jsx
@@ -0,0 +1,17 @@
+const Cartao = (props) => {
+    return(
+        // .card>(.card-header.text-muted+.card-body)
+        <div className="card">
+            <div className="card-header text-muted">
+                {props.cabecalho}
+            </div>
+            <div className="card-body">
+                {props.children}   
+            </div>
+        </div>
+    )
+}
+
+
+
+export default Cartao
