0ba6ad6095bdfcba3805faa144716157f061eb90
Rodrigo Bossini <professorbossini@gmail.com>
2025-03-15 10:52:02 -0300

    learn(04-react): encerra a apostila

--- componentes-props/src/App.jsx
+++ componentes-props/src/App.jsx
@@ -1,47 +1,73 @@
 import Pedido from "./Pedido"
+import Cartao from "./Cartao"
+import Feedback from "./Feedback"
 const App = () => {
+  const textoOK = 'Já chegou'
+  const textoNOK = 'Não chegou ainda'
+  const funcaoOK = () => alert('Agradecemos a confirmação')
+  const funcaoNOK = () => alert('Verificaremos o ocorrido')
+  const componenteFeedback = (
+    <Feedback 
+      textoOK={textoOK}
+      textoNOK={textoNOK}
+      funcaoOK={funcaoOK}
+      funcaoNOK={funcaoNOK}
+    />
+  )
   return (
     // div.rotulo.titulo#abc{oi}[style=""]
     <div className="container border mt-2">
       <div className="row">
 
-        <div className="col-lg-6 col-xxl-3">
+        <div className="col-lg-6 col-xxl-4">
 
-          <Pedido
-            data={new Date().toDateString()}
-            titulo="SSD"
-            descricao="Um SSD de 512Gb"
-            icone="hdd" />
+          <Cartao
+            cabecalho={new Date().toDateString()}>
+            <Pedido
+              titulo="SSD"
+              descricao="Um SSD de 512Gb"
+              icone="hdd" />
+              {componenteFeedback}
+          </Cartao>
 
-        </div>
-
-        <div className="col-lg-6 col-xxl-3">
-
-          <Pedido
-            data={new Date().toDateString()}
-            titulo="Livro"
-            descricao="Harry Potter"
-            icone="book" />
 
         </div>
 
-        <div className="col-lg-6 col-xxl-3">
+        <div className="col-lg-6 col-xxl-4">
 
+          <Cartao
+            cabecalho={new Date().toDateString()}>
+            <Pedido
+              titulo="Livro"
+              descricao="Harry Potter"
+              icone="book" />
+              {componenteFeedback}
+          </Cartao>
+
+        </div>
+
+        <div className="col-lg-6 col-xxl-4">
+          
+          <Cartao
+            cabecalho={new Date().toDateString()}>
           <Pedido
-            data={new Date().toDateString()}
             titulo="Hipopótamo"
             descricao="Sim, um hipo! :)"
             icone="hippo" />
+            {componenteFeedback}
+          </Cartao>
 
         </div>
 
-        <div className="col-lg-6 col-xxl-3">
-
-          <Pedido
-            data={new Date().toDateString()}
-            titulo="Um boneco de neve"
-            descricao="Um boneco de neve que balança sozinho"
-            icone="snowman fa-shake" />
+        <div className="col-lg-6 col-xxl-4">
+          <Cartao
+            cabecalho={new Date().toDateString()}>
+            <Pedido
+              titulo="Um boneco de neve"
+              descricao="Um boneco de neve que balança sozinho"
+              icone="snowman fa-shake" />
+              {componenteFeedback}
+          </Cartao>
 
         </div>
 
--- componentes-props/src/Pedido.jsx
+++ componentes-props/src/Pedido.jsx
@@ -1,8 +1,6 @@
 const Pedido = (props) => {
     return (
-        <div className="card">
-            <div className="card-header text-muted">{props.data}</div>
-            <div className="card-body d-flex">
+            <div className="d-flex">
                 <div className="d-flex justify-content-center flex-column">
                     <i className={`fa-solid fa-${props.icone} fa-2x`}></i>
                 </div>
@@ -11,7 +9,6 @@ const Pedido = (props) => {
                     <p className="text-center">{props.descricao}</p>
                 </div>
             </div>
-        </div>
     )
 }
 
