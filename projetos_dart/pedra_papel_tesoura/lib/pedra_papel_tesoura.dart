745956bd935d6e8c3025a2ecfe2ad6791c0844e9
Rodrigo Bossini <professorbossini@gmail.com>
2025-08-30 11:09:57 -0300

    learn(ap02-dart): encerra a apostila

--- projetos_dart/pedra_papel_tesoura/lib/pedra_papel_tesoura.dart
+++ projetos_dart/pedra_papel_tesoura/lib/pedra_papel_tesoura.dart
@@ -1,28 +1,57 @@
+import 'dart:io';
+import 'dart:math';
 //usar enum
+enum OPCAO {pedra, papel, tesoura, sair}
+
+void exibir(String texto){
+  print(texto);
+}
+
+int pegarOpcaoUsuario(){
+  return int.parse(stdin.readLineSync()!);  
+}
+
+bool opcaoEhValida(int opcao){
+  return (opcao >= 1 && opcao <=4);
+}
+
+//o values do enum é um vetor que tem todos os valores previstos no enum
+//opcao varia de 1 a 4
+OPCAO mapearOpcao(int opcao){
+  return OPCAO.values[opcao - 1];
+}
+
+String decidirResultado(OPCAO opcaoUsuario, OPCAO opcaoComputador){
+  if(opcaoUsuario == opcaoComputador) return "Empate";
+  const voceVenceu = "Você venceu";
+  if(opcaoUsuario == OPCAO.pedra && opcaoComputador == OPCAO.tesoura) return voceVenceu;
+  if(opcaoUsuario == OPCAO.papel && opcaoComputador == OPCAO.pedra) return voceVenceu;
+  if(opcaoUsuario == OPCAO.tesoura && opcaoComputador == OPCAO.papel) return voceVenceu;
+  return "Computador venceu";
+}
 //gerar a opção do computador aleatoriamente
 //usuario pode escolher o que deseja
+
 void jogo(){
-  //enquanto o usuário não quiser sair
-    //exibir menu
+  int opUsuarioInt;
+  OPCAO opUsuario, opComputador;
+  do{
+    do{
+      exibir('1-Pedra\n2-Papel\n3-Tesoura\n4-Sair');
+      opUsuarioInt = pegarOpcaoUsuario();
+    }while(!opcaoEhValida(opUsuarioInt));
+    opUsuario = mapearOpcao(opUsuarioInt);
+    switch(opUsuario){
+      case OPCAO.sair:
+        exibir("Até!");
+      default:
+        opComputador = mapearOpcao(Random().nextInt(3) + 1);
+        //Você(pedra) VS (tesoura)Computador
+        exibir('Você(${opUsuario.name}) VS (${opComputador.name})Computador');
+        final resultado = decidirResultado(opUsuario, opComputador);
+        exibir(resultado);
+        sleep(Duration(seconds: 3));
+    }
 
-    // capturar a opção do usuário, validando
-
-    //se o usuário escolher sair, sair
-
-    //senão
-
-      //sortear a escolha do computador
-
-      //mapear a opção do usuário de int para enum
-
-      //mapear a opção do computador de int para enum
-
-      //exibir as opções de cada um
-      //Você(pedra) VS (papel)Computador
-
-      //decidir quem venceu ou se houve empate
-
-      //exibir  o resultado
-
-      //dormir por 4 segundos
+  }while(opUsuario != OPCAO.sair);
 }
