a4e0bdf3b1a3ac94ecdabc6ab8f37b09b193339b
Rodrigo Bossini <professorbossini@gmail.com>
2025-08-30 12:30:12 -0300

    learn(ap03-dart): pg 7
    - jogo da mega sena com lista proposto, nao resolvido em aula
    - alunos resolveram e mostraram solucoes no dontpad

---
+++ projetos_dart/colecoes/bin/colecoes.dart
@@ -0,0 +1,35 @@
+import 'package:colecoes/colecoes.dart' as colecoes;
+
+void main(List<String> arguments) {
+
+
+  //type annotation
+  // var nomes = <String?> ["a", null];
+  // const nomes = ['João', 'Ana'];
+  // nomes.add('Pedro');
+  // nomes[0] = 'João Silva';
+  // final a;
+  // print(a);
+  // var a;
+  // print(a);
+  // List <dynamic> a = [];
+  // var lista = [];
+  // List <dynamic> nomes = ['Ana', 'Pedro'];
+  // nomes.add(true);
+  // nomes.add(1);
+  // nomes[0].falar();
+  // nomes.add(null);
+
+  // List<Object?> outra = ['whatever', 1];
+  // outra.add(true);
+  // outra.add(null);
+  // print(outra[0].hashCode);
+  //var itensDiversos = [1, 1.2, true, "abc"];
+  //for/in
+  //enhanced
+  // for(final item in itensDiversos){
+  //   print(item);
+  // }
+  // var numeros = [1, 1.2];
+  // var nomes = ['João', 'Pedro'];
+}
